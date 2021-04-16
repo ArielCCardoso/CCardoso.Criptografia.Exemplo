62b59a19de55857d5c3dac878b91002a92f47c7f
Ariel C. Cardoso <arielccardoso@live.com>
2021-04-16 00:09:51 -0300

    Cria projeto

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+
+namespace CCardoso.Criptografia.Exemplo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
